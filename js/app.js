09dc5dd12c97c08326e337f92924d64d117d8bb8
Ronson Shiigi <ronsonshiigi@Ronsons-MacBook-Pro.local>
2018-10-23 01:47:56 -1000

    1

--- js/app.js
+++ js/app.js
@@ -2,6 +2,25 @@
 //Add event listeners to the following:
 
 /*1) to the navmenu items (Home, Location, Blog) that will show and hide the contents for that specific navmenu item.*/
+//class = navi
+//class to reveal is class = inner
+var barNav = document.getElementsByClassName('navi');
+for(var i=0; i<barNav.length; i++){
+    barNav[i].addEventListener('click',poof)
+}
+function poof(){
+    // console.log(this.innerHTML)
+    var bigReveal = this.querySelectorAll('.inner')[0];
+    if(bigReveal.style.display === 'block'){
+        bigReveal.style.display = 'none';
+    }else{bigReveal.style.display = 'block'}
+}
+
+
+
+
+
+
 
 
 /*2) to each restaurant name that will show and hide (toggle) the secret menu item for that particular restaurant.*/
